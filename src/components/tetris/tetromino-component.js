d500f21171243bc1657cbbf49c525e00da67797c
Porkopek <peresnegro@gmail.com>
2017-02-02 13:24:45 +0100

    test

--- src/components/tetris/tetromino-component.js
+++ src/components/tetris/tetromino-component.js
@@ -17,24 +17,7 @@ export const Tetromino = (props: Props) => {
   const {tetrominoesColors} = props.settings
   return (
     <div>    
-      {props.matrix.map((row, rowIndex) =>
-        row.map((column, columnIndex) => {
-          const content = props.matrix[rowIndex][columnIndex]
-          return (
-            <Block
-              backgroundColor={content !== 0 ? tetrominoesColors[content - 1] : 'rgba(0,0,0,.1)'}
-              className={content !== 0 ? props.className : 'transparent-block'}
-              settings={props.settings}
-              key={columnIndex + ' ' + rowIndex}
-              index={{ column: props.column + columnIndex, row: props.row + rowIndex }}
-              content={content}
-              id={"id" + columnIndex + rowIndex}
-              row={props.row + rowIndex}
-              column={props.column + columnIndex}
-              style={{zIndex:content !== 0 ?1000 : 1000}}
-            />)
-        })
-      )}
+     test
     </div>
   )
 
